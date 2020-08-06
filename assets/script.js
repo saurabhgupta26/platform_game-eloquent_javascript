44e2550d95cc250f525388c5721957d0472d93e1
Saurabh <saurabhguptaviet@gmail.com>
2020-08-07 00:01:27 +0530

    initial commit with initial files

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+class Level {
+  constructor(plan) {
+    let rows = plan
+      .trim()
+      .split("\n")
+      .map((l) => [...l]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch];
+        if (typeof type == "string") return type;
+        this.startActors.push(type.create(new Vec(x, y), ch));
+        return "empty";
+      });
+    });
+  }
+}
