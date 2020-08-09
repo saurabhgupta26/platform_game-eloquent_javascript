07e4701dc288111ba9b094c5d9f7f166eb905ed0
Saurabh <saurabhguptaviet@gmail.com>
2020-08-10 03:05:52 +0530

    Good to go with functioning, will go for canvas

--- levels.js
+++ levels.js
@@ -1,5 +1,5 @@
 console.log("in level");
-var LEVEL = [`                                                    
+var GAME_LEVELS = [`                                                    
 ................................................................................
 ................................................................................
 ................................................................................
@@ -173,7 +173,7 @@ var LEVEL = [`
 ..............................................................................................................
 `];
 
-// if (typeof module != "undefined" && module.exports && (typeof window == "undefined" || window.exports != exports))
-//   module.exports = GAME_LEVELS;
-// if (typeof global != "undefined" && !global.GAME_LEVELS)
-//   global.GAME_LEVELS = GAME_LEVELS;
+if (typeof module != "undefined" && module.exports && (typeof window == "undefined" || window.exports != exports))
+  module.exports = GAME_LEVELS;
+if (typeof global != "undefined" && !global.GAME_LEVELS)
+  global.GAME_LEVELS = GAME_LEVELS;
